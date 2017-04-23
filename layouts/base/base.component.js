11f5fcc3003e2f245684183af0cd6e16f4accaa2
Thanh Tran <vanthanh2305@gmail.com>
2017-04-23 10:39:26 +0700

    Add encapsulation && host

--- layouts/base/base.component.js
+++ layouts/base/base.component.js
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: '{{baseComponentFile}}',
   templateUrl: './templates/{{baseComponentFile}}.component.html',
   styleUrls: ['./styles/{{baseComponentFile}}.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: '{{baseComponentFile}}',
+  },
 })
 export class {{baseComponent}}Component {
   constructor() { }
